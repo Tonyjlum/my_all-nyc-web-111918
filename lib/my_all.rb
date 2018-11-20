17f6b089f74d5e26a920d28febde65c0d6b58933
Tonyjlum <tonyjlum@gmail.com>
2018-11-19 22:44:34 -0500

    I gave it my_all

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i +=1
+  end
 
+  block_return_values.each {|el| return false if el == false}
+  true
 end
